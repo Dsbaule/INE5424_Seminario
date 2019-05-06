6f85b777e9460522d43ef64f2ec3debd366c5340
Daniel Baulé <dsbaule@gmail.com>
2019-05-06 02:04:13 -0300

    Split line in .sh

--- APB/run.sh
+++ APB/run.sh
@@ -6,7 +6,8 @@ arm-none-eabi-gcc -c -mcpu=cortex-a9 -g src/APB_test.c -o obj/APB_test.o
 arm-none-eabi-gcc -c -mcpu=cortex-a9 -g src/APB_Slave.c -o obj/APB_Slave.o
 arm-none-eabi-gcc -c -mcpu=cortex-a9 -g src/APB_Master.c -o obj/APB_Master.o
 
-arm-none-eabi-ld -T src/test.ld obj/APB_Slave.o obj/APB_Master.o obj/APB_test.o obj/startup.o -o obj/test.elf
+arm-none-eabi-ld -T src/test.ld \
+    obj/APB_Slave.o obj/APB_Master.o obj/APB_test.o obj/startup.o -o obj/test.elf
 
 arm-none-eabi-objcopy -O binary obj/test.elf obj/test.bin
 
