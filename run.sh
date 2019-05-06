f12f3c39abfa4070874c1fbb2a47d8475e6baaee
Daniel Baulé <dsbaule@gmail.com>
2019-05-06 09:20:59 -0300

    run.sh arrumado

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 if [ $# -eq 0 ]; then
-      echo "Invalid argument, use (APT | ATB | AHB)"
-      echo "Example: bash run.sh APT"
+    echo "Invalid argument, use (APB | ATB | AHB)"
+    echo "Example: bash run.sh APB"
 elif [ $1 = "APB" ]; then
     cd APB
     bash run.sh
@@ -11,6 +11,6 @@ elif [ $1 = "AHB" ]; then
     cd AHB
     bash run.sh
 else
-    echo "Invalid argument, use (APT | ATB | AHB)"
-    echo "Example: bash run.sh APT"
+    echo "Invalid argument, use (APB | ATB | AHB)"
+    echo "Example: bash run.sh APB"
 fi
